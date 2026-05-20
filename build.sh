d0a56c9ac9092f47a2bb129a0081ea05593d81b3
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-05-20 18:33:22 -0400

    a

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 haxe dox.hxml
-haxelib run dox -i docs -o pages --title "Psych Engine Documentation" -D source-path https://github.com/ShadowMario/FNF-PsychEngine/tree/main/source
+haxelib run dox -i docs -o pages --title "Psych Engine Haxe API" -D source-path https://github.com/ShadowMario/FNF-PsychEngine/tree/main/source
