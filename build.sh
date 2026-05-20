61d685dde2cf6eca03385ba741c1efe371e77a12
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-05-20 18:24:06 -0400

    init

---
+++ build.sh
@@ -0,0 +1,2 @@
+haxe dox.hxml
+haxelib run dox -i docs -o pages --title "Psych Engine Documentation" -D source-path https://github.com/ShadowMario/FNF-PsychEngine/tree/main/source
---
+++ libraries.sh
@@ -0,0 +1,15 @@
+mkdir .haxelib
+haxelib --always install dox
+haxelib --always install flixel 5.6.1
+haxelib --always install flixel-addons 3.2.2
+haxelib --always install flixel-tools 1.5.1
+haxelib --always install hscript-iris 1.1.3
+haxelib --always install tjson 1.4.0
+haxelib --always install hxdiscord_rpc 1.2.4
+haxelib --always install hxvlc 2.0.1 --skip-dependencies
+haxelib --always install lime 8.1.2
+haxelib --always install openfl 9.3.3
+haxelib --always git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
+haxelib --always git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
+haxelib --always git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
+haxelib --always git grig.audio https://github.com/FunkinCrew/grig.audio refactor/fft-cam-version
